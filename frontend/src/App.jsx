207e6222691fa8406b9f2803a5a051b305363817
Sandesh Gupta <sandeshgupta3867@gmail.com>
2026-06-19 22:07:09 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import SignUp from "./pages/SignUp.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import Customize from "./pages/Customize.jsx";
 import { userDataContext } from "./context/UserContext.jsx";
-import Home from "./pages/home.jsx";
+import Home from "./pages/Home.jsx";
 import Customize2 from "./pages/Customize2.jsx"; 
 
 function App() {
